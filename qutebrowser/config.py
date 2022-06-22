4e8a14b20b67eb93e286be7f0a9c889f9ec4171d
16underscore <steffen.gminsky@online.de>
2022-06-22 22:01:06 +0200

    edit qutebrowser config

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -39,11 +39,14 @@ c.tabs.tooltips = False
 c.url.default_page = 'about:blank'
 c.url.searchengines = {
 	'DEFAULT': 'https://duckduckgo.com/?q={}',
+	'ac': 'https://adventofcode.com/{}',
 	'al': 'https://archlinux.org/packages/?q={}',
+	'cr': 'https://crates.io/search?q={}',
 	'dc': 'https://www.dict.cc/?s={}',
+	'iv': 'https://invidious.lunar.icu/search?q={}'
 	'mw': 'https://minecraft.fandom.com/wiki/{}',
+	'os': 'https://odysee.com/$/search?q={}',
 	'rd': 'https://reddit.com/r/{}',
-	'yt': 'https://youtube.com/results?search_query={}',
 	'lh': 'localhost:{}'
 }
 
